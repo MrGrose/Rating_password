6aed4412d88ad307ef2b69377c3bb879a98c3d8a
MrGrose <147311692+MrGrose@users.noreply.github.com>
2024-11-25 21:39:49 +0300

    Add files via upload

--- rating_password.py
+++ rating_password.py
@@ -1,28 +1,32 @@
 import urwid
 
+POSITIVE = 2
+NEGATIVE = 0
+
 
 def is_very_long(password: str) -> int:
-    return 2 if len(password) >= 12 else 0
+    return POSITIVE if len(password) >= 12 else NEGATIVE
 
 
 def has_digit(password: str) -> int:
-    return 2 if any(elem.isdigit() for elem in password) else 0
+    return POSITIVE if any(elem.isdigit() for elem in password) else NEGATIVE
 
 
 def has_letters(password: str) -> int:
-    return 2 if any(elem.isalpha() for elem in password) else 0
+    return POSITIVE if any(elem.isalpha() for elem in password) else NEGATIVE
 
 
 def has_upper_letters(password: str) -> int:
-    return 2 if any(elem.isupper() for elem in password) else 0
+    return POSITIVE if any(elem.isupper() for elem in password) else NEGATIVE
 
 
 def has_lower_letters(password: str) -> int:
-    return 2 if any(elem.islower() for elem in password) else 0
+    return POSITIVE if any(elem.islower() for elem in password) else NEGATIVE
 
 
 def has_symbols(password: str) -> int:
-    return 2 if any((elem == '#' or elem == '%') for elem in password) else 0
+    return POSITIVE if any((elem == '#' or elem == '%')
+                           for elem in password) else NEGATIVE
 
 
 def on_ask_change(edit: urwid.Edit, text: str) -> None:
@@ -39,7 +43,7 @@ def on_ask_change(edit: urwid.Edit, text: str) -> None:
 
 
 def on_ask_exit(key: str) -> None:
-    if key in ('esc'):
+    if key in 'esc':
         raise urwid.ExitMainLoop()
 
 
